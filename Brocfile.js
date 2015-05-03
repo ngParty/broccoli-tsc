ac21bb1e740f4c66cbc673cd25d9e6497ae0aa81
Mario Vejlupek <mario@vejlupek.cz>
2015-05-03 02:09:13 +0200

    Initial plugin implementation

---
+++ Brocfile.js
@@ -0,0 +1,9 @@
+// Broc file for test purpose
+
+var compileTypeScript = require( './' );
+
+var app = '.';
+
+app = compileTypeScript( app );
+
+module.exports = app;
---
+++ index.ts
@@ -0,0 +1,44 @@
+/// <reference path='typings/tsd.d.ts' />
+
+//TODO: add d.ts definition
+var Filter = require( 'broccoli-filter' )
+var typescript = require( 'typescript' )
+
+class TypeScriptFilter extends Filter {
+
+  constructor( inputTree: string, options: any = {} ) {
+
+    // Ensure instance of object on regular call
+    if ( !(this instanceof TypeScriptFilter) ) {
+
+      return new TypeScriptFilter(inputTree, options)
+
+    }
+
+    super( inputTree, options )
+
+  }
+
+  extensions = [ 'ts' ]
+  targetExtension = 'js'
+
+  processString = function( string, srcFile ) {
+
+    var typeScriptOptions = { module: typescript.ModuleKind.CommonJS };
+
+    try {
+
+      return typescript.transpile( string, typeScriptOptions )
+
+    } catch (err) {
+
+      // TODO: implement error reporting
+      throw err
+
+    }
+  }
+
+}
+
+
+module.exports = TypeScriptFilter
